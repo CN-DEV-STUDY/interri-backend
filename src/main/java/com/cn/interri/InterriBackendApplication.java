f3bc59822b5ae50e8358945f2623ea5d040842fe
chaesuhyeon <ddd8177@gmail.com>
2023-04-23 22:24:00 +0900

    feat: init commit

---
+++ src/main/java/com/cn/interri/InterriBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cn.interri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterriBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterriBackendApplication.class, args);
+	}
+
+}
