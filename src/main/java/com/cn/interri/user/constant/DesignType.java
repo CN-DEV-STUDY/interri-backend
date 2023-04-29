d9f1048b1411306df481273d3240c100354539a5
nohyunha95 <nohyunha95@gmail.com>
2023-04-29 12:53:04 +0900

    CN-7 feat: DesignType enum 생성 & Scrab entity 생성

---
+++ src/main/java/com/cn/interri/user/constant/DesignType.java
@@ -0,0 +1,6 @@
+package com.cn.interri.user.constant;
+
+public enum DesignType {
+    REQ, // 디자인 요청
+    RES, // 디자인 응답
+}
---
+++ src/main/java/com/cn/interri/user/domain/User/Scrab.java
@@ -0,0 +1,33 @@
+package com.cn.interri.user.domain.User;
+
+import com.cn.interri.user.constant.DesignType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "scrab")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Scrab {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "scrab_id")
+    @Comment("스크랩 id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @Comment("사용자")
+    private User user;
+
+    @Comment("디자인 id")
+    private Long designId;
+
+    @Enumerated(EnumType.STRING)
+    @Comment("디자인 타입")
+    private DesignType designType;
+}
