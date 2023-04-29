bb623a166d7fee71cd12d759dcd1ece05b29dba5
nohyunha95 <nohyunha95@gmail.com>
2023-04-29 12:37:25 +0900

    CN-7 feat: user & design_req 엔티티 생성

--- src/main/java/com/cn/interri/design/domain/DesignRes.java
+++ src/main/java/com/cn/interri/design/domain/DesignRes.java
@@ -1,9 +1,11 @@
 package com.cn.interri.design.domain;
 
+import com.cn.interri.common.BaseTimeEntity;
 import com.cn.interri.user.domain.User.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
 
@@ -11,12 +13,32 @@ import javax.persistence.*;
 @Table(name = "design_res")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class DesignRes {
+public class DesignRes extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "design_res_id")
+    @Comment("디자인 응답 id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @JoinColumn(name = "user_id", nullable = false)
+    @Comment("사용자")
+    private User user; // 사용자
+
+    @Column(nullable = false)
+    @Comment("응답 가격")
+    private Integer price; // 응답 가격
+
+    @Column(length = 500, nullable = false)
+    @Comment("응답 내용")
+    private String content;
+
+    @Column(length = 1, nullable = false)
+    @Comment("삭제 여부")
+    private String delYn;
+
+    @Comment("조회수")
+    private Integer viewCnt;
+
+    @Comment("스크랩 수")
+    private Integer scrabCnt;
 }
--- src/main/java/com/cn/interri/user/domain/User/User.java
+++ src/main/java/com/cn/interri/user/domain/User/User.java
@@ -1,5 +1,6 @@
 package com.cn.interri.user.domain.User;
 
+import com.cn.interri.common.BaseTimeEntity;
 import com.cn.interri.user.constant.SignType;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -11,28 +12,25 @@ import javax.persistence.*;
 @Table(name = "user")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long id;
+    private Long id; // 사용자 id
 
     @Column(length = 20, nullable = false)
-    private String email;
+    private String email; // 사용자 이메일
 
     @Column(length = 20, nullable = false)
-    private String password;
+    private String password; // 사용자 비밀번호
 
     @Enumerated(EnumType.STRING)
     @Column(length = 1, nullable = false)
-    private SignType signType;
+    private SignType signType; // 가입 타입
 
     @Column(length = 50)
-    private String address;
+    private String address; // 사용자 주소
 
     @Column(length = 20)
-    private String phone;
-
-
-
+    private String phone; // 사용자 전화번호
 }
