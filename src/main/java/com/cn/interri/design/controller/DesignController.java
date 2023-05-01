31fc685ec3bb7d7ba93a2854dadf33888f410508
nohyunha95 <nohyunha95@gmail.com>
2023-05-01 22:43:47 +0900

    CN-8 feat: 컨트롤러 커밋

--- src/main/java/com/cn/interri/design/controller/DesignController.java
+++ src/main/java/com/cn/interri/design/controller/DesignController.java
@@ -1,15 +1,37 @@
 package com.cn.interri.design.controller;
 
+import com.cn.interri.common.dto.ResponseDto;
+import com.cn.interri.design.dto.ReqRegistrationResource;
+import com.cn.interri.design.service.PageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/design")
 public class DesignController {
 
-    @GetMapping("/design/{id}/details")
+    private final PageService pageService;
+
+    @GetMapping("/req")
+    public ResponseEntity<ResponseDto<ReqRegistrationResource>> getRegistrationPage() {
+        ReqRegistrationResource resource = pageService.getRegistrationPageResource();
+
+        return ResponseEntity.ok()
+                .body(ResponseDto.<ReqRegistrationResource>builder()
+                        .data(resource)
+                        .build());
+    }
+
+    @GetMapping("/req/{id}/details")
     public void getDesignReqDetails() throws Exception {
 
     }
+
+
 }
