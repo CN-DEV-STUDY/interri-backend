d11ac76eb65a3a36d3f8b4bbf1a3cc7056e25e54
chaesuhyeon <ddd8177@gmail.com>
2023-05-06 16:59:55 +0900

    CN-9 fix: imgurl s3 bucket url로 수정

--- src/main/java/com/cn/interri/design/service/impl/PageServiceImpl.java
+++ src/main/java/com/cn/interri/design/service/impl/PageServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cn.interri.design.service.impl;
 
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.cn.interri.design.domain.FileDesignRes;
 import com.cn.interri.design.dto.*;
 import com.cn.interri.design.enums.Colors;
@@ -8,6 +9,7 @@ import com.cn.interri.design.repository.custom.DesignResRepository;
 import com.cn.interri.design.service.PageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +30,11 @@ public class PageServiceImpl implements PageService {
     private final DesignReqInfoRepository designReqInfoRepository;
     private final DesignResRepository designResRepository;
     private final FileDesignResRepository fileDesignResRepository;
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
 
     @Override
     public ReqRegistrationResource getRegistrationPageResource() {
@@ -52,22 +59,30 @@ public class PageServiceImpl implements PageService {
     @Override
     public ReqDetailReqResource getDesignReqDetails(Long id) {
 
+        // 디자인 요청 내용
         ReqDetailReqResource reqDetail = designReqRepository.getReqDetail(id);
         List<ReqInfoDetailResource> reqInfoDetail = designReqInfoRepository.getReqInfoDetail(id);
-        List<ReqDetailResResource> reqDetailRes = designResRepository.getReqDetailRes(id);
 
+        reqInfoDetail.stream().map(this::getUrl).collect(Collectors.toList()); // s3 imgUrl로 update
+
+        // 디자인 요청에 대한 답변 내용
+        List<ReqDetailResResource> reqDetailRes = designResRepository.getReqDetailRes(id);
         List<ReqDetailResResource> reqDetailResList = reqDetailRes.stream().map(res -> {
             FileDesignRes fileDesignRes = fileDesignResRepository.findByDesignRes_IdAndRepYn(res.getId(), "Y");
 
-            res.setFilePath(fileDesignRes.getFilePath());
-            res.setFileNm(fileDesignRes.getFileNm());
+            res.setFilePath(amazonS3Client.getUrl(bucket,fileDesignRes.getFilePath()).toString());
             return res;
         }).collect(Collectors.toList());
-
 
         reqDetail.setReqInfoDetailResources(reqInfoDetail);
         reqDetail.setReqDetailResResources(reqDetailResList);
 
         return reqDetail;
+    }
+
+    private ReqInfoDetailResource getUrl(ReqInfoDetailResource rep) {
+        String imgUrl = amazonS3Client.getUrl(bucket, rep.getImgPath()).toString();
+        rep.setImgPath(imgUrl);
+        return rep;
     }
 }
