ed6d498f576ce6c465494802659420a4a2f0aeb0
chaesuhyeon <ddd8177@gmail.com>
2023-05-06 15:39:08 +0900

    CN-16 feat: s3 bucket 설정 및 연결

--- src/main/java/com/cn/interri/InterriBackendApplication.java
+++ src/main/java/com/cn/interri/InterriBackendApplication.java
@@ -8,6 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class InterriBackendApplication {
 
+	static {
+		// 이 설정을 하지 않은 경우 서비스가 실행되는 시점에 약간의 지연이 발생하고 예외가 발생한다.
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(InterriBackendApplication.class, args);
 	}
