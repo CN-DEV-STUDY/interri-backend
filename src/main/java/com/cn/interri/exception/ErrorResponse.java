b8458ced50bcfc479d2b5c85e0bb50366c936036
nohyunha95 <nohyunha95@gmail.com>
2023-05-13 17:10:43 +0900

    CN-27 feat: exception 설정 추가

---
+++ src/main/java/com/cn/interri/exception/ErrorResponse.java
@@ -0,0 +1,52 @@
+package com.cn.interri.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class ErrorResponse {
+
+    private LocalDateTime timestamp = LocalDateTime.now();
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @JsonProperty("errors")
+    private List<CustomFieldError> customFieldErrors;
+
+    @Builder
+    public ErrorResponse(HttpStatus httpStatus, String message, List<FieldError> fieldErrors) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+
+        //BindingResult.getFieldErrors() 메소드를 통해 전달받은 fieldErrors
+        for (FieldError fieldError : fieldErrors) {
+            this.customFieldErrors.add(
+                    CustomFieldError.builder()
+                            .field(fieldError.getField())
+                            .value(fieldError.getRejectedValue())
+                            .reason(fieldError.getDefaultMessage())
+                            .build()
+            );
+        }
+    }
+
+    /**
+     * parameter 검증에 통과하지 못한 필드가 담긴 클래스
+     */
+    @Builder
+    @Getter
+    public static class CustomFieldError {
+        private String field;
+        private Object value;
+        private String reason;
+
+    }
+}
